1f1035b60ee8c473b937f4c4e7d57172fabdf7cb
marcosmarinalopez <marcosmarinalopez@outlook.es>
2023-04-26 21:32:55 +0200

    adding post method. Must be tested

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, jsonify
+from flask import Flask, g, jsonify, request
 import psycopg2
 
 app = Flask(__name__)
@@ -42,6 +42,7 @@ def home():
 
     return home_display
 
+
 @app.route('/books', methods=['GET'])
 def get_books():
     conn = fx__get_db()
@@ -51,6 +52,31 @@ def get_books():
     cursor.close()
     
     return jsonify(totalRows)
+
+
+@app.route('/resources/book/add', methods=['POST'])
+def add_book():
+    message = "El libro se ha añadido correctamente"
+    book = request.get_json()
+
+    author = book['author']
+    year = book['year']
+    title = book['title']
+    description = book['description']
+    conn = fx__get_db()
+
+    try:
+        conn = fx__get_db()
+        cursor = conn.cursor()
+        cursor.execute('insert into books_table (author, year, title, description) values (%s, %s, %s, %s), (author, year, title, description)')
+        cursor.close()
+        conn.commit()
+    except Exception as ex: 
+        conn.rollback()
+        message = "Error al añadir el libro"
+    
+    return jsonify({message})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
